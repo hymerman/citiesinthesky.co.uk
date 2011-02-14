6d7f7bbc66c736d24e0300492e422e86a479fb20
Ben Hymers <b.hymers@gmail.com>
2011-02-14 20:34:02 +0000

    Finished off redirection configuration

--- config.py
+++ config.py
@@ -2,39 +2,16 @@ import os
 import logging
 
 # Default URL: if no Redirect URL can be generated, traffic will be diverted here
-DEFAULT_URL = 'http://www.benhymers.com/'
-
-# Mapping of Source URLs to Target URLs.
-# You can choose to map in 2 ways:
-#    1) to the new URL maintaining the same Path (i.e. 'www.old.com/p/a/t/h/index.html -> www.new.com/p/a/t/h/index.html')
-#    2) or to discard the Path (i.e. 'www.old.com/p/a/t/h/index.html -> www.new.com')
-# Format:
-#    URLS = { 'Old Domain': ('New Domain', 'Discard URL Path part and Redirect to Root?'), ... }
+DEFAULT_URL = 'http://blog.benhymers.com/'
 
 # todo:
-# www.citiesinthesky.co.uk -> new page
-# www.citiesinthesky.co.uk/cities-in-the-sky -> new page
-
-# www.citiesinthesky.co.uk/#/#/#/# -> blog.benhymers.com/#/#/#/#
-# www.citiesinthesky.co.uk/page/# -> blog.benhymers.com/page/#
-
-# www.citiesinthesky.co.uk/category/# -> blog.benhymers.com/tag/#
-# www.citiesinthesky.co.uk/category/#/page/# -> blog.benhymers.com/tag/#/#
-
-# www.citiesinthesky.co.uk/about-ben -> www.benhymers.com
-# www.citiesinthesky.co.uk/employers-look-here -> www.benhymers.com
-
-# and same for all bare domains
-
-URLS = {
-	'www.citiesinthesky.com' : ('www.new-domain.com', False),
-	'downloads.old-domain.com': ('downloads.new-domain.com', False),
-	'stuff.old-domain.com': ('stuff.new-domain.com', True),
-};
+# Handle /category/x/page/n properly
+# Handle funny characters in /nnnn/nn/nn/title-with-hyphens properly
+# Handle archives like /2010/06 properly
 
 # Send email message when redirect error occurs
 ERROR_EMAIL_ACTIVE = True;
-ERROR_EMAIL_SENDER = '"Redirector for new-domain.com" <your@email.com>';
+ERROR_EMAIL_SENDER = '"Redirector for www.citiesinthesky.co.uk" <b.hymers@gmail.com>';
 ERROR_EMAIL_SUBJECT = 'Redirect Script Error';
 ERROR_EMAIL_BODY = 'Unable to redirect this url: ';
 
--- redirect.py
+++ redirect.py
@@ -17,104 +17,110 @@ import config
 
 # Check if a URL exists, trying a URL Fetch
 def check_url_exists(url):
-   # Search the Memcache
-   memcachekey = "check_url_exists-" + url;
-   result = memcache.get(memcachekey) if config.MEMCACHE_ACTIVE else None;
-   
-   # Nothing in Memcache
-   if ( result == None ):
-      logging.debug("Existence of URL '%s' not in Memcache" % (url) );
-      try:
-         response = urlfetch.fetch(url, allow_truncated=True, deadline=10); # Wait as much as possible: 10 sec
-         if ( response.status_code == 200 ):
-            logging.debug("Verified URL '%s' existence" % (url));
-            result = True;
-         else:
-            logging.error("Unable to Verify URL '%s' existence" % (url));
-            result = False;
-      except:
-         # In case of InvalidURLError or DownloadError
-         logging.error("Exception while Verifying URL '%s' existence" % (url));
-         result = False;
-      
-      # Store the result in Memcache for config.MEMCACHE_EXPIRES_IN_SECONDS
-      memcache.set(memcachekey, result, time=config.MEMCACHE_EXPIRES_IN_SECONDS);
-   
-   return result;
+	# Search the Memcache
+	memcachekey = "check_url_exists-" + url;
+	result = memcache.get(memcachekey) if config.MEMCACHE_ACTIVE else None;
 
+	# Nothing in Memcache
+	if ( result == None ):
+		logging.debug("Existence of URL '%s' not in Memcache" % (url) );
+		try:
+			response = urlfetch.fetch(url, allow_truncated=True, deadline=10); # Wait as much as possible: 10 sec
+			if ( response.status_code == 200 ):
+				logging.debug("Verified URL '%s' existence" % (url));
+				result = True;
+			else:
+				logging.error("Unable to Verify URL '%s' existence" % (url));
+				result = False;
+		except:
+			# In case of InvalidURLError or DownloadError
+			logging.error("Exception while Verifying URL '%s' existence" % (url));
+			result = False;
+
+		# Store the result in Memcache for config.MEMCACHE_EXPIRES_IN_SECONDS
+		memcache.set(memcachekey, result, time=config.MEMCACHE_EXPIRES_IN_SECONDS);
+
+	return result;
+
+def remove_trailing_forward_slash(string):
+	if string[-1:] == "/":
+		return string[:-1]
+
+	return string
 
 # Generates the URL to redirect to
 def get_redirect_url(url):
-   # Search the Memcache
-   memcachekey = "get_redirect_url-" + url;
-   result = memcache.get(memcachekey) if config.MEMCACHE_ACTIVE else None;
-   
-   # Nothing in Memcache
-   if ( result == None ):
-      logging.debug("Redirect for URL '%s' not in Memcache" % (url) );
-      scheme, netloc, path, query, fragment = urlparse.urlsplit(url);
-	
-      # Discard any port number from the hostname
-      netloc = netloc.split(':', 1)[0];
-	
-   	# Fix empty paths to be just '/' for consistency
-      if path == '':
-         path = '/';
-	
-   	# Check if we have a mapping for this domain
-      if netloc in config.URLS:
-         # Grab the redirect info tuple
-         redirect_info = config.URLS[netloc];
-         # Root redirects
-         if redirect_info[1]:
-            result = 'http://' + redirect_info[0] + '/';
-            logging.debug("Redirecting to Root: " + result);
-         # Paths
-         else:
-            result = urlparse.urlunsplit(['http', redirect_info[0], path, query, fragment]);
-            logging.debug("Redirecting to Precise Path: " + result);
-      # No mapping, so return None
-      else:
-         logging.debug("No Mapping found for: " + url);
-         result = None;
-   	
-      # Store the result in Memcache for config.MEMCACHE_EXPIRES_IN_SECONDS
-      memcache.set(memcachekey, result, time=config.MEMCACHE_EXPIRES_IN_SECONDS);
+	# Search the Memcache
+	memcachekey = "get_redirect_url-" + url;
+	result = memcache.get(memcachekey) if config.MEMCACHE_ACTIVE else None;
 
-   return result;
+	# Nothing in Memcache
+	if ( result == None ):
+		logging.debug("Redirect for URL '%s' not in Memcache" % (url) )
+		scheme, netloc, path, query, fragment = urlparse.urlsplit(url)
+	
 		
+		# Fix empty paths to be just '/' for consistency
+		if path == '':
+			path = '/'
+
+		# Check for /page/n
+		if path.startswith('/page'):
+			result = 'http://blog.benhymers.com' + remove_trailing_forward_slash(path)
+
+		# Check for /category/x
+		# Take everything from the 10th character (after /category/)
+		# Ignore /category/x/page/n since it's hard and nobody really visits it anyway...
+		elif path.startswith('/category'):
+			result = 'http://blog.benhymers.com/tag/' + remove_trailing_forward_slash(path[10:])
+
+		elif path.startswith('/about-ben') or path.startswith('/employers-look-here'):
+			result = 'http://www.benhymers.com'
+
+		# Even redirect to the main page; we don't want it to look like this page still exists
+		elif path.startswith('/cities-in-the-sky'):
+			result = 'http://www.citiesinthesky.co.uk'
+
+		# Assume that any other path is an actual page, in which case the mapping is direct
+		# Forget about the hyphen character issue for now since it's confusing
+		else:
+			result = 'http://blog.benhymers.com' + remove_trailing_forward_slash(path) + '/'
+
+		# Store the result in Memcache for config.MEMCACHE_EXPIRES_IN_SECONDS
+		memcache.set(memcachekey, result, time=config.MEMCACHE_EXPIRES_IN_SECONDS)
+
+	return result;
+
 
 # Main Request Handler
 class MainHandler(webapp.RequestHandler):
-   def get(self):
-      # Perform redirect
-      url = get_redirect_url(self.request.url);
+	def get(self):
+		# Perform redirect
+		url = get_redirect_url(self.request.url);
 
-      # Check that we were able to build a URL and that this URL actually exists
-      if url and (check_url_exists(url) if config.CHECK_URL_EXISTANCE else True):
-         logging.info("Redirecting URL '%s' to URL '%s'" % (self.request.url, url) );
-         self.redirect(url, permanent=True);
-      else:
-         # Log that we didn't know what this was, and redirect to a good default
-         logging.error("Unable to Redirect URL '%s'" % (self.request.url) );
-         if config.ERROR_EMAIL_ACTIVE:
-            mail.send_mail_to_admins(
-               sender=config.ERROR_EMAIL_SENDER,
-               subject=config.ERROR_EMAIL_SUBJECT,
-               body=config.ERROR_EMAIL_BODY + self.request.url
-            );
-         self.redirect(config.DEFAULT_URL, permanent=True);
+		# Check that we were able to build a URL and that this URL actually exists
+		if url and (check_url_exists(url) if config.CHECK_URL_EXISTANCE else True):
+			logging.info("Redirecting URL '%s' to URL '%s'" % (self.request.url, url) );
+			self.redirect(url, permanent=True);
+		else:
+			# Log that we didn't know what this was, and redirect to a good default
+			logging.error("Unable to Redirect URL '%s'" % (self.request.url) );
+			if config.ERROR_EMAIL_ACTIVE:
+				mail.send_mail_to_admins(
+					sender=config.ERROR_EMAIL_SENDER,
+					subject=config.ERROR_EMAIL_SUBJECT,
+					body=config.ERROR_EMAIL_BODY + self.request.url
+				);
+			self.redirect(config.DEFAULT_URL, permanent=True);
 
 
 application = webapp.WSGIApplication([("/.*", MainHandler)], debug=True);
 
 
 def main():
-   logging.getLogger().setLevel(config.LOGGING_LEVEL);
-   run_wsgi_app(application);
+	logging.getLogger().setLevel(config.LOGGING_LEVEL);
+	run_wsgi_app(application);
 
 
 if __name__ == "__main__":
-   main();
-
-   
+	main();
