d00e26094569fa9300e534f272cc2e449a400073
Ben Hymers <b.hymers@gmail.com>
2011-05-22 15:10:19 +0100

    Fixed redirecting last post from old blog

--- redirect.py
+++ redirect.py
@@ -83,7 +83,7 @@ def get_redirect_url(url):
 		if path == '':
 			path = '/'
 
-		if path == '/2010/03/02/another-fresh-start-but-not-here/':
+		if remove_trailing_forward_slash(path) == '/2010/03/02/another-fresh-start-but-not-here':
 			logging.debug("Found last post from blog, not migrated, so redirecting to root")
 			result = 'http://blog.benhymers.com'
 
